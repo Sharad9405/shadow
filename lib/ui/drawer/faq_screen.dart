8e350c6c01ebb31087543e6664d7c541a2ef9604
Sharad Pawar <p.sharad9405@gmail.com>
2021-01-20 20:41:48 +0530

    webview implementation

--- lib/ui/drawer/faq_screen.dart
+++ lib/ui/drawer/faq_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shadow/config/config.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FAQScreen extends StatefulWidget {
   const FAQScreen({Key key}) : super(key: key);
@@ -8,15 +14,16 @@ class FAQScreen extends StatefulWidget {
   _FAQScreenState createState() => _FAQScreenState();
 }
 
-class _FAQScreenState extends State<FAQScreen>
-    with TickerProviderStateMixin {
+class _FAQScreenState extends State<FAQScreen> with TickerProviderStateMixin {
   AnimationController animationController;
+  final Completer<WebViewController> _webViewController = Completer<WebViewController>();
 
   @override
   void initState() {
     animationController = AnimationController(
         duration: const Duration(milliseconds: 600), vsync: this);
     super.initState();
+    if(Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
@@ -24,22 +31,119 @@ class _FAQScreenState extends State<FAQScreen>
     animationController.dispose();
     super.dispose();
   }
-
+  // padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppTheme.white,
-      body: Padding(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_appBar()],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppTheme.white,
+        body: Padding(
+          padding: EdgeInsets.all(0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              _appBar(),
+
+              Expanded(
+                child: Stack(
+                  children: [
+                    _setUpWebView(),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child:  Padding(
+                        padding: EdgeInsets.only(bottom: 20,right: 20),
+                        child: _favoriteButton(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  _favoriteButton(){
+    return FutureBuilder<WebViewController>(
+          future: _webViewController.future,
+      builder: (BuildContext context, AsyncSnapshot<WebViewController> controller){
+            if(controller.hasData){
+              return FloatingActionButton(
+                onPressed: () async {
+                  final String url = await controller.data.currentUrl();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Favorited $url'),));
+                },
+                child: const Icon(Icons.favorite),
+              );
+            }
+            return Container();
+      },
+    );
+  }
+
+  _setUpWebView(){
+    return Container(
+      child: Builder(builder: (BuildContext context){
+        return WebView(
+          initialUrl: 'https://flutter.dev',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController){
+            _webViewController.complete(webViewController);
+          },
+          // TODO(iskakaushik): Remove this when collection literals makes it to stable.
+          // ignore: prefer_collection_literals
+
+          javascriptChannels: <JavascriptChannel>[
+            _toasterJavaScriptChannel(context)
+          ].toSet(),
+
+          // these will help to detect gesture for map only start
+          gestureRecognizers: {
+            Factory<OneSequenceGestureRecognizer>(
+                  () => EagerGestureRecognizer(),
+            ),
+          },
+
+
+          navigationDelegate: (NavigationRequest request){
+            if(request.url.startsWith('https://www.youtube.com/')){
+              print('blocking navigation to $request');
+              return NavigationDecision.prevent;
+            }
+            print('Allowing navigation to $request');
+            return NavigationDecision.navigate;
+        },
+
+          onPageStarted: (String url){
+            print('Page started loading.... $url');
+          },
+
+          onPageFinished: (String url){
+            print('Page finished loading ... $url');
+          },
+
+          gestureNavigationEnabled: true,
+        );
+      }),
+    );
+  }
+
+  JavascriptChannel _toasterJavaScriptChannel(BuildContext context){
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message){
+        // ignore: deprecated_member_use
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(message.message),
+          ));
+          print('JavascriptChannel => onMessageReceived() => ${message.message}');
+        }
+    );
+  }
 
   Widget _appBar() {
     return SizedBox(
--- lib/ui/drawer/offers_screen.dart
+++ lib/ui/drawer/offers_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:shadow/config/config.dart';
+
+int numItems = 10;
+var onSelectCard = ValueNotifier<int>(0);
 
 class OffersScreen extends StatefulWidget {
   const OffersScreen({Key key}) : super(key: key);
@@ -27,19 +33,37 @@ class _OffersScreenState extends State<OffersScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppTheme.white,
-      body: Padding(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_appBar()],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppTheme.white,
+        body: Padding(
+          padding: EdgeInsets.all(0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _appBar(),
+
+              Container(
+                // decoration: BoxDecoration(
+                //     color: Colors.transparent,
+                //     gradient: LinearGradient(
+                //       begin: Alignment.topLeft,
+                //       end: Alignment.bottomRight,
+                //       colors: [Color(0xff74ABE4), Color(0xffA892ED)],
+                //       stops: [0,1],
+                //     )
+                // ),
+                child: Center(
+                    child: MyScener(),
+                  ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
 
   Widget _appBar() {
     return SizedBox(
@@ -65,5 +89,144 @@ class _OffersScreenState extends State<OffersScreen>
         ],
       ),
     );
+  }
+}
+
+class CardData {
+  Color color;
+  double x, y, z, angle;
+  final int idx;
+  double alpha = 0;
+
+  Color get lightColor {
+    var val = HSVColor.fromColor(color);
+    return val.withSaturation(.5).withValue(.8).toColor();
+  }
+
+  CardData(this.idx) {
+    color = Colors.primaries[idx % Colors.primaries.length];
+    x = 0;
+    y = 0;
+    z = 0;
+  }
+}
+
+class MyScener extends StatefulWidget {
+  @override
+  _MyScenerState createState() => _MyScenerState();
+}
+
+class _MyScenerState extends State<MyScener> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  List<CardData> cardData = [];
+  int numItems = 9;
+  double radio = 200.0;
+  double radioStep;
+  int centerIdx = 1;
+
+  @override
+  void initState() {
+    cardData = List.generate(numItems, (index) => CardData(index)).toList();
+    radioStep = (pi * 2) / numItems;
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _animationController.addListener(() => setState(() {}));
+    _animationController.addStatusListener((status) async {
+      if (status == AnimationStatus.completed) {
+        _animationController.value = 0;
+        _animationController.animateTo(1);
+        ++centerIdx;
+      }
+    });
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var ratio = _animationController.value;
+    double animValue = centerIdx + ratio;
+    // process positions.
+    for (var i = 0; i < cardData.length; ++i) {
+      var c = cardData[i];
+      double ang = c.idx * radioStep + animValue;
+      c.angle = ang + pi / 2;
+      c.x = cos(ang) * radio;
+      // c.y = sin(ang) * 10;
+     // c.y = sin(ang) * 30;
+      c.z = sin(ang) * radio;
+    }
+
+    // sort in Z axis.
+    cardData.sort((a, b) => a.z.compareTo(b.z));
+
+    var list = cardData.map((vo) {
+      var c = addCard(vo);
+      var mt2 = Matrix4.identity();
+      mt2.setEntry(3, 2, 0.001);
+      mt2.translate(vo.x, vo.y, -vo.z);
+      mt2.rotateY(vo.angle + pi);
+      c = Transform(
+        alignment: Alignment.center,
+        origin: Offset(0.0, -0.0),
+        transform: mt2,
+        child: c,
+      );
+
+      // depth of field... doesnt work on web.
+     // var blur = .4 + ((1 - vo.z / radio) / 2) * 2;
+     // c = BackdropFilter(
+     //   filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+     //   child: c,
+     // );
+
+      return c;
+    }).toList();
+
+    return Container(
+      alignment: Alignment.center,
+      child: Stack(
+        alignment: Alignment.center,
+        children: list,
+      ),
+    );
+  }
+
+  Widget addCard(CardData vo) {
+    var alpha = ((1 - vo.z / radio) / 2) * .6;
+    Widget c;
+    c = Container(
+      margin: EdgeInsets.all(12),
+      // width: 120,
+      // height: 80,
+      width: 120,
+      height: 120,
+      alignment: Alignment.center,
+      foregroundDecoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.black.withOpacity(alpha),
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.1, .9],
+          colors: [vo.lightColor, vo.color],
+        ),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(.2 + alpha * .2),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(0, 2))
+        ],
+      ),
+      child: Text('ITEM ${vo.idx}'),
+    );
+    return c;
   }
 }
